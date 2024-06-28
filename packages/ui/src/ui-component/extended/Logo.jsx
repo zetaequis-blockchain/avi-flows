6c9c0148aaa011147231cf629a5da7e82b3e7aba
francisco-zx <franciscoaboy@gmail.com>
2024-06-28 17:57:26 -0300

    update theme

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -11,7 +11,7 @@ const Logo = () => {
     return (
         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
             <img
-                style={{ objectFit: 'contain', height: 'auto', width: 150 }}
+                style={{ objectFit: 'contain', width: 'auto', height: 35 }}
                 src={customization.isDarkMode ? logoDark : logo}
                 alt='Flowise'
             />
